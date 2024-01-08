5571dbbc33d7daec0b71a3df34b00abe975e5ac1
kxxhyorim <rlagyfla0709@gmail.com>
2024-01-08 20:30:55 +0900

    [Lec03] Async의 start 옵션을 LAZY로 지정할 경우와 await()의 관계

--- src/main/kotlin/coroutine/Lec03.kt
+++ src/main/kotlin/coroutine/Lec03.kt
@@ -5,85 +5,84 @@ import kotlin.system.measureTimeMillis
 
 
 fun main(): Unit = runBlocking {
-  val time = measureTimeMillis {
-    val job1 = async(start = CoroutineStart.LAZY) { apiCall1() }
-    val job2 = async(start = CoroutineStart.LAZY) { apiCall2() }
+    val time = measureTimeMillis {
+        val job1 = async(start = CoroutineStart.LAZY) { apiCall1() }
+        val job2 = async(start = CoroutineStart.LAZY) { apiCall2() }
 
-    job1.start()
-    job2.start()
-    printWithThread(job1.await() + job2.await())
-  }
+        job1.start()
+        job2.start()
+        printWithThread(job1.await() + job2.await())
+        /* async의 start 파라미터로 LAZY를 지정한 경우
+         start()를 명시적으로 호출하지 않는다면
+         await()가 호출될 때에서야 코루틴이 실행된다. */
+    }
 
-  printWithThread("소요 시간 : $time ms")
+    printWithThread("소요 시간 : $time ms")
 }
 
 suspend fun apiCall1(): Int {
-  delay(1_000L)
-  return 1
+    delay(1_000L)
+    return 1
 }
 
 suspend fun apiCall2(): Int {
-  delay(1_000L)
-  return 2
+    delay(1_000L)
+    return 2
 }
 
 
-
-
-
-
 fun example5(): Unit = runBlocking {
-  val job = async {
-    3 + 5
-  }
-  val eight = job.await() // await : async의 결과를 가져오는 함수
-  printWithThread(eight)
+    val job = async {
+        3 + 5
+    }
+    val eight = job.await() // await : async의 결과를 가져오는 함수
+    printWithThread(eight)
 }
 
 
 fun example4(): Unit = runBlocking {
-  val job1 = launch {
-    delay(1_000)
-    printWithThread("Job 1")
-  }
-  job1.join()
+    val job1 = launch {
+        delay(1_000)
+        printWithThread("Job 1")
+    }
+    job1.join()
 
-  val job2 = launch {
-    delay(1_000L)
-    printWithThread("Job 2")
-  }
+    val job2 = launch {
+        delay(1_000L)
+        printWithThread("Job 2")
+    }
 }
 
 
 fun example3(): Unit = runBlocking {
-  val job = launch {
-    (1..5).forEach {
-      printWithThread(it)
-      delay(500)
+    val job = launch {
+        (1..5).forEach {
+            printWithThread(it)
+            delay(500)
+        }
     }
-  }
 
-  delay(1_000L)
-  job.cancel()
+    delay(1_000L)
+    job.cancel()
 }
 
 fun example2(): Unit = runBlocking {
-  val job = launch(start = CoroutineStart.LAZY) {
-    printWithThread("Hello launch")
-  }
+    val job = launch(start = CoroutineStart.LAZY) {
+        printWithThread("Hello launch")
+    }
 
-  delay(1_000L)
-  job.start()
+    delay(1_000L)
+    job.start()
 }
 
 fun example1() {
-  runBlocking {
-    printWithThread("START")
-    launch {
-      delay(2_000L) // yield()
-      printWithThread("LAUNCH END")
+    runBlocking {
+        printWithThread("START")
+        launch {
+            delay(2_000L) // yield()
+            printWithThread("LAUNCH END")
+        }
     }
-  }
 
-  printWithThread("END")
+    printWithThread("END")
 }
